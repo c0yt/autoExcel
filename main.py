ab5e60f9dda29cfe06902c52ca9cad65a5bd6947
c0yt <167622633+c0yt@users.noreply.github.com>
2024-08-12 15:08:08 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 import openpyxl
 import threading
 import os
+import sys
 import fnmatch
 from openpyxl.styles import Border, Side
 
-# 设置控制台输出颜色
 GREEN = '\033[92m'
 RED = '\033[91m'
 RESET = '\033[0m'
@@ -54,10 +54,10 @@ class Excel_Process:
 //             佛祖保佑       永不宕机      永无BUG               //
 //                                                                //
 //@Author null-sky                                                //
-//@Date 2024/7/20                                                 //
+//@Date 2024/8/12                                                 //
 //@GitHub https://github.com/c0yt                                 //
-//@version 7.0                                                    //
-//@Name Auto_Excel                                            //
+//@version 8.0                                                    //
+//@Name Auto_Excel                                                //
 ////////////////////////////////////////////////////////////////////
 """
         print(f"{GREEN}{pattern}{RESET}")
@@ -66,7 +66,7 @@ class Excel_Process:
         try:
             if not os.path.exists(self.data_file):
                 print(f"{RED}[-]data.txt不存在！")
-                exit(-1)
+                sys.exit()
             with open(self.data_file, 'r', encoding='ansi') as f:
                 content = f.read()
             utf8_content = content.encode('utf-8')
@@ -103,7 +103,7 @@ class Excel_Process:
 
             if self.flag_cnt != 3:
                 print(f"{RED}[-]当前目录存在多个重名文件或文件缺失！{RESET}")
-                exit(-1)
+                sys.exit()
 
             self.analytics_sheet = self.analytics_workbook[self.analytics_workbook.sheetnames[0]]
             self.total_sheet = self.total_workbook[self.total_workbook.sheetnames[0]]
@@ -124,7 +124,7 @@ class Excel_Process:
 
         except Exception as e:
             print(f"{RED}[-]文件名不匹配或不存在！请关闭文件后重试！{RESET}")
-            exit(-1)
+            sys.exit()
 
     class MyThread(threading.Thread):
         def __init__(self, func, args=()):
@@ -136,25 +136,54 @@ class Excel_Process:
             self.result = self.func(*self.args)
 
         def get_result(self):
-            threading.Thread.join(self)  # 等待线程执行完毕
+            threading.Thread.join(self)
             try:
                 return self.result
             except Exception:
                 return None
 
-    def find_student_id(self, name):
+    def find_student_id(self, class_id, name):
+        matched_students = []
         for student in self.total_list:
-            if name in student:
-                return student[0]
-        print(f"{RED}[-]未找到该名字：{name}{RESET}")
-        exit(-1)
+            if student[0] == class_id:
+                if student[2] == name:
+                    matched_students.append(student)
+            elif matched_students:
+                break
+        if len(matched_students) == 0:
+            print(f"{RED}[-]未找到：{class_id}班{name}{RESET}")
+            sys.exit()
+        elif len(matched_students) == 1:
+            return matched_students[0][1]
+        else:
+            print(f"{RED}[!]{class_id}班存在多个同名学生：{name}{RESET}")
+            print("+" + "-" * 30 + "+")
+            print(f"| {'序号':<3} | {'学号':<9} | {'姓名':<6} |")
+            print("+" + "-" * 30 + "+")
+
+            for i, student in enumerate(matched_students, 1):
+                print(f"| {i:<4} | {student[1]:<10} | {student[2]:<6} |")
+
+            print("+" + "-" * 30 + "+")
+
+            while True:
+                try:
+                    choice = int(input(f"[!]请选择正确的学号（输入1-{len(matched_students)}的数字）："))
+                    if 1 <= choice <= len(matched_students):
+                        return matched_students[choice - 1][1]
+                    else:
+                        print(f"{RED}[-]输入不合法，请输入1-{len(matched_students)}之间的数字{RESET}")
+                except ValueError:
+                    print(f"{RED}[-]输入不合法，请输入一个数字{RESET}")
 
     def add_to_list(self, record, flag):
         records = record.split(',')
+        class_id = None
         for item in records:
             parts = item.split(":")
             if parts[0][0].isdigit():
-                thread = self.MyThread(self.find_student_id, (parts[1],))
+                class_id = int(parts[0][0:4])
+                thread = self.MyThread(self.find_student_id, (class_id, parts[1]))
                 thread.start()
                 thread.join()
                 if flag == 1:
@@ -164,7 +193,7 @@ class Excel_Process:
                     self.late_list.append(int(parts[0][0:4]))
                     self.late_list.append([thread.get_result()] + parts[1:])
             else:
-                thread = self.MyThread(self.find_student_id, (parts[0],))
+                thread = self.MyThread(self.find_student_id, (class_id, parts[0]))
                 thread.start()
                 thread.join()
                 if flag == 1:
@@ -229,9 +258,9 @@ class Excel_Process:
     def process_data_file(self):
         try:
             print("[+]获取总名单")
-            for row in self.total_sheet.iter_rows(min_row=4, max_row=self.total_list_max_row, min_col=2, max_col=3):
+            for row in self.total_sheet.iter_rows(min_row=4, max_row=self.total_list_max_row, min_col=1, max_col=3):
                 if str(row[0].value).isdigit():
-                    self.total_list.append([row[0].value, row[1].value])
+                    self.total_list.append([row[0].value, row[1].value, row[2].value])
 
             try:
                 with open(self.data_file, 'r', encoding='utf-8') as f:
@@ -258,7 +287,6 @@ class Excel_Process:
             try:
                 print("[+]写入周表")
                 week_max_row = self.week_sheet.max_row - 16
-                # 重置表格
                 if week_max_row != 4:
                     while week_max_row > 4:
                         self.week_sheet.delete_rows(4)
@@ -271,7 +299,6 @@ class Excel_Process:
                 self.write_week_table(self.absent_list, 1)
                 self.write_week_table(self.late_list, 0.25)
 
-                # 定义边框样式
                 border_style = Border(
                     left=Side(border_style='thin'),
                     right=Side(border_style='thin'),
@@ -279,12 +306,10 @@ class Excel_Process:
                     bottom=Side(border_style='thin')
                 )
 
-                # 定义要应用边框的范围
                 start_row = 4
                 end_row = self.week_sheet.max_row - 17
                 start_col = 1
                 end_col = self.week_sheet.max_column -9
-                # 应用边框到指定范围的每个单元格
                 for row in self.week_sheet.iter_rows(min_row=start_row, max_row=end_row, min_col=start_col, max_col=end_col):
                     for cell in row:
                         cell.border = border_style
@@ -294,7 +319,7 @@ class Excel_Process:
             except Exception as e:
                 print(f"{RED}[-]周表写入失败！{RESET}")
                 print(f"{RED}[-]请检查data.txt")
-                exit(-1)
+                sys.exit()
 
             start_row = 3
             start_column = 3
@@ -318,7 +343,7 @@ class Excel_Process:
             except Exception as e:
                 print(f"{RED}[-]总表写入失败！{RESET}")
                 print(f"{RED}[-]请检查data.txt")
-                exit(-1)
+                sys.exit()
 
             updated_rows = 0
             print("[+]更新总表数据")
@@ -332,14 +357,13 @@ class Excel_Process:
 
             print(f"{GREEN}[+]更新总表数据完成，已更新{updated_rows}条数据！{RESET}")
 
-            # 处理统计表和保存文件
             self._write_analytics_sheet()
             self.week_workbook.save(self.current_week_path)
             self.total_workbook.save(self.current_total_path)
 
         except Exception as e:
             print(f"{RED}[-]process_data_file->出错!{RESET}")
-            exit(-1)
+            sys.exit()
 
     def _write_analytics_sheet(self):
         class_dict = {}
